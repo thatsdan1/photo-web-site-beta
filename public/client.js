175ce38af73c2b49ac1f8d5a16ee95ace6c56f59
Daniel Odetoye <dodetoye@icloud.com>
2024-06-02 17:12:28 -0400

    putting everything together

--- public/client.js
+++ public/client.js
@@ -20,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.querySelector(hash).scrollIntoView({
                     behavior: 'smooth'
                 });
+                // Close the mobile menu after clicking a link
                 menu.classList.remove('is-active');
                 menuLinks.classList.remove('active');
             }
@@ -56,14 +57,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         slideIndex++;
         if (slideIndex > slides.length) { slideIndex = 1 }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
         slides[slideIndex - 1].style.display = "block";
-        if (dots[slideIndex - 1]) {
-            dots[slideIndex - 1].className += " active";
+        slides[slideIndex - 1].classList.add("active");
+        if (dots.length > 0) {
+            for (i = 0; i < dots.length; i++) {
+                dots[i].classList.remove("active");
+            }
+            dots[slideIndex - 1].classList.add("active");
         }
-        setTimeout(showSlides, 2000);
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
     }
 
     // Handle form submission
@@ -78,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function() {
             date: bookingForm.date.value,
         };
 
-        fetch('/api/submit-form', {
+        fetch('http://localhost:8000/booking', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
