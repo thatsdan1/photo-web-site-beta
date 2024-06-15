f98b2488afd09bf59b37442a17236daa0d019e6b
Daniel Odetoye <dodetoye@icloud.com>
2024-06-15 02:14:35 -0400

    Fixed client.js and ensured slideshow functionality

--- public/client.js
+++ public/client.js
@@ -1,107 +1,102 @@
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import nodemailer from 'nodemailer';
-import bookingRoutes from './booking.js'; // Import your booking routes
+document.addEventListener("DOMContentLoaded", function() {
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.navbar__menu');
+    const bookButtons = document.querySelectorAll('.book-btn');
+    const modal = document.getElementById('bookingModal');
+    const closeBtn = document.querySelector('.close-btn');
+    const bookingForm = document.getElementById('bookingForm');
 
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 8000;
-const MONGO_URL = process.env.MONGO_URL;
-
-mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Database connected successfully.'))
-  .catch(err => console.error('Database connection error:', err));
-
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-// Routes
-app.use('/api/booking', bookingRoutes); // Use booking routes
-
-mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Database connected successfully.');
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    })
-    .catch(error => {
-        console.error('Failed to connect to MongoDB:', error);
-        process.exit(1); // Exit the process with failure
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
     });
 
-const bookingSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    occasion: { type: String, required: true },
-    email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
-    phone: { type: Number, required: true, min: 1000000000, max: 9999999999 },
-    date: { type: String, required: true, match: /^\d{4}-\d{2}-\d{2}$/ }
-// Serve static files
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-const Booking = mongoose.model('Booking', bookingSchema);
-
-app.post('/api/booking', async (req, res) => {
-    const { name, occasion, email, phone, date } = req.body;
-    console.log('Received booking request:', req.body);
-
-    try {
-        const existingBooking = await Booking.findOne({ email, date });
-        if (existingBooking) {
-            console.error('Booking already exists for this email and date.');
-            return res.status(400).send('Booking already exists for this email and date.');
-        }
-
-        const newBooking = new Booking({ name, occasion, email, phone, date });
-        await newBooking.save();
-        res.status(200).send('Booking added successfully!');
-
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
-
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: process.env.ALERT_EMAIL,
-            subject: 'New Booking Submission',
-            text: `New booking received:\n\nName: ${name}\nOccasion: ${occasion}\nEmail: ${email}\nPhone: ${phone}\nDate: ${date}`,
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error('Error sending email:', error);
-            } else {
-                console.log('Email sent: ' + info.response);
+    // Smooth scroll
+    document.querySelectorAll('.navbar__links, .button').forEach(link => {
+        link.addEventListener('click', function(e) {
+            if (this.hash !== "") {
+                e.preventDefault();
+                const hash = this.hash;
+                document.querySelector(hash).scrollIntoView({
+                    behavior: 'smooth'
+                });
+                // Close the mobile menu after clicking a link
+                menu.classList.remove('is-active');
+                menuLinks.classList.remove('active');
             }
         });
+    });
 
-    } catch (err) {
-        console.error('Error submitting booking:', err);
-        if (err.name === 'ValidationError') {
-            return res.status(400).send('Validation Error: ' + err.message);
+    // Modal functionality
+    bookButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            modal.style.display = 'block';
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (e) => {
+        if (e.target == modal) {
+            modal.style.display = 'none';
         }
-        res.status(500).send('Error: ' + err);
+    });
+
+    // Slide show functionality
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        const slides = document.getElementsByClassName("mySlides");
+        const dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        slides[slideIndex - 1].style.display = "block";
+        slides[slideIndex - 1].classList.add("active");
+        if (dots.length > 0) {
+            for (i = 0; i < dots.length; i++) {
+                dots[i].classList.remove("active");
+            }
+            dots[slideIndex - 1].classList.add("active");
+        }
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
     }
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+    // Handle form submission
+    bookingForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const formData = {
+            name: bookingForm.name.value,
+            occasion: bookingForm.occasion.value,
+            email: bookingForm.email.value,
+            phone: bookingForm.phone.value,
+            date: bookingForm.date.value,
+        };
+
+        fetch('/api/booking', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        }).then(response => {
+            if (response.ok) {
+                alert('Booking submitted successfully');
+                modal.style.display = 'none';
+                bookingForm.reset();
+            } else {
+                alert('Error submitting booking');
+            }
+        }).catch(error => {
+            console.error('Error:', error);
+            alert('Error submitting booking');
+        });
+    });
 });
-
-
-
-
-
